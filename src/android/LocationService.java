bed695817447b844e881f25c2c7ac9db7b0329d0
Toni Korin <toni.korin@gmail.com>
2015-07-08 15:54:19 +0300

    -HTTP address support -Code formatting -Push Server interface in case of collision

--- src/android/LocationService.java
+++ src/android/LocationService.java
@@ -27,10 +27,14 @@ import android.os.Bundle;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.os.SystemClock;
 import android.util.Log;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
 import java.util.Iterator;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
@@ -44,15 +48,21 @@ import java.text.SimpleDateFormat;
 import com.tonikorin.cordova.plugin.LocationProvider.MyLocation;
 import com.tonikorin.cordova.plugin.LocationProvider.MyLocation.LocationResult;
 
-public class LocationService extends IntentService {
+import static java.lang.Math.*;
 
-    public static final String PREFS_NAME = "LocationService";
-    private static final String TAG = "Cordova " + PREFS_NAME;
-    public static final String CONFIG_NAME = "config";
-    public static final String HISTORY_NAME = "history";
+public class LocationService extends IntentService
+{
+    private static final String TAG = "LocationService";
     private final MyLocationResult myLocationResult = new MyLocationResult();
     private JSONObject config = null;
     private Context myContext;
+    public static final String PREFS_NAME = "LocationService";
+    public static final String CONFIG_NAME  = "config";
+    public static final String HISTORY_NAME = "history";
+    public static final String ALIVE    = "ALIVE";
+    public static final String POSITION = "POSITION";
+    public static final String FAILURE  = "FAILURE";
+    public static final String RESERVED = "RESERVED";
 
     public LocationService() {
         super("LocationService");
@@ -61,11 +71,14 @@ public class LocationService extends IntentService {
     // The IntentService calls this method from the default worker thread with the intent that
     // started the service. When this method returns, IntentService stops the service.
     @Override
-    protected void onHandleIntent(Intent intent) {
-        try {
+    protected void onHandleIntent(Intent intent)
+    {
+        try
+        {
             myContext = LocationService.this;
             // read the configuration
-            if (this.config == null) {
+            if (this.config == null)
+            {
                 String jsonString = myContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).getString(CONFIG_NAME, "{}");
                 config = new JSONObject(jsonString);
             }
@@ -74,13 +87,15 @@ public class LocationService extends IntentService {
             String msgJsonStr = extras.getString("data"); // own "data" property inside "data" property
             JSONObject messageIn = new JSONObject(msgJsonStr);
             handleLocationQuery(messageIn);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             Log.e(TAG, "onHandleIntent exception ", e);
         }
     }
 
-    private void handleLocationQuery(JSONObject messageIn) throws JSONException, IOException {
-
+    private void handleLocationQuery(JSONObject messageIn) throws JSONException, IOException
+    {
         Log.d(TAG, "Handle location query...");
         // Store details about location query
         updateLocateHistory(messageIn);
@@ -89,7 +104,8 @@ public class LocationService extends IntentService {
         JSONObject teams = config.optJSONObject("teams");
         String teamName = "";
         String teamPassword = "";
-        for (Iterator<String> iter = teams.keys(); iter.hasNext(); ) {
+        for (Iterator<String> iter = teams.keys(); iter.hasNext(); )
+        {
             String team = iter.next();
             if (team.equals(messageIn.optString("teamId"))) {
                 teamName = teams.optJSONObject(team).optString("name", "");
@@ -98,40 +114,42 @@ public class LocationService extends IntentService {
             }
         }
         if (ownName.equals("") || teamName.equals(""))
-            return; // => URI hanging in PostLocator
-        // Messaging Server interface
+            return; // => URI hanging in PostServer
+        // Create Messaging Server interface
         MessageServer msgServer = new MessageServer(ownName, teamName, teamPassword, config.optString("messageUrl", ""));
-        if (messageIn.optString("memberName").equals(ownName)) {
-            msgServer.post("RESERVED");
-            /* todo
-            PushLocator pushLocator = new PushLocator(ownName, messageIn.teamId, "");
-            // 5 sec delay
-            pushLocator.updateToken();
-            */
+        if (messageIn.optString("memberName").equals(ownName))
+        {
+            msgServer.post(RESERVED);
+            SystemClock.sleep(5000);// 5 sec delay
+            msgServer.updatePushToken(ownName, teamName, config.optString("token", ""), config.optString("pushUrl", ""));
             return;
         }
-        msgServer.post("ALIVE");
-        try {
+        msgServer.post(ALIVE);
+        try
+        {
             //Log.d(TAG, "myContext: " + myContext.getPackageName());
             new MyLocation(myContext, myLocationResult, messageIn.optInt("accuracy"), 110 /* sec timeout */).start();
             JSONObject location = myLocationResult.getJsonLocation();
             //Log.d(TAG, "Background position accuracy: " + location.optInt("accuracy"));
-            msgServer.post("POSITION", location.toString());
-        } catch (Exception e) {
+            msgServer.post(POSITION, location.toString());
+        }
+        catch (Exception e)
+        {
             Log.e(TAG, "LocationProvider exception ", e);
-            msgServer.post("FAILURE", e.getMessage());
+            msgServer.post(FAILURE, e.getMessage());
         }
         Log.d(TAG, "Handle location query...completed!");
     }
 
-    private String getDateAndTimeString(long utcTime) {
+    private String getDateAndTimeString(long utcTime)
+    {
         Date date = new Date(utcTime);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(date);
     }
 
-    private void updateLocateHistory(JSONObject messageIn) throws JSONException {
-        // Read current history
+    private void updateLocateHistory(JSONObject messageIn) throws JSONException
+    {   // Read current history
         SharedPreferences sp = myContext.getSharedPreferences(LocationService.PREFS_NAME, Context.MODE_PRIVATE);
         String historyJsonStr = sp.getString(LocationService.HISTORY_NAME, "{}");
         JSONObject history = new JSONObject(historyJsonStr);
@@ -162,31 +180,48 @@ public class LocationService extends IntentService {
         //Log.d(TAG, "history:" + history.toString());
     }
 
-    private class MessageServer {
-
-        private HttpsURLConnection con;
+    private class MessageServer
+    {
+        private HttpURLConnection con;
         private JSONObject messageOut;
         private OutputStream os;
         private String xChannel;
         private String urlString;
+        private static final String TOKEN = "TOKEN";
         //private InputStream is;
 
-        public MessageServer(String ownName, String team, String password, String urlString) throws JSONException, IOException {
-            // create message template
+        public MessageServer(String ownName, String teamName, String teamPassword, String urlMessageServer) throws JSONException, IOException
+        {  // Create message template for location data
             this.messageOut = new JSONObject();
             this.messageOut.put("memberName", ownName);
-            this.messageOut.put("teamId", team);
-            this.xChannel = "/channel/" + team + ":" + password;
-            this.urlString = urlString;
+            this.messageOut.put("teamId", teamName);
+            this.xChannel = "/channel/" + teamName + ":" + teamPassword;
+            this.urlString = urlMessageServer;
         }
 
-        public void post(String messageType) throws JSONException, IOException {
+        public void updatePushToken(String ownName, String teamName, String pushToken, String urlPushServer) throws JSONException, IOException
+        {   // Create message template for push server and post it
+            this.messageOut = new JSONObject();
+            this.messageOut.put("name", ownName);
+            this.messageOut.put("team", teamName);
+            this.messageOut.put("token", pushToken);
+            this.messageOut.put("tokenType", 3); // GCM
+            this.urlString = urlPushServer;
+            this.post(TOKEN);
+        }
+
+        public void post(String messageType) throws JSONException, IOException
+        {
             Log.d(TAG, "POST: " + messageType);
-            messageOut.put("messageType", messageType);
-            try {
-                // Create connection and request
+            if( !messageType.equals(TOKEN) )// messageType property not needed in PushServer
+                messageOut.put("messageType", messageType);
+            try
+            {   // Create connection and request
                 URL url = new URL(urlString);
-                con = (HttpsURLConnection) url.openConnection();
+                if(url.getProtocol().equals("https"))
+                    con = (HttpsURLConnection) url.openConnection();
+                else
+                    con = (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("POST");
                 con.setDoOutput(true); // = POST method
                 //con.setDoInput(true);
@@ -199,36 +234,42 @@ public class LocationService extends IntentService {
                 os.write(messageOut.toString().getBytes("UTF-8"));
                 os.flush();
                 Log.d(TAG, "POST response code:" + con.getResponseCode());
-            } finally {
+            }
+            finally
+            {
                 os.close();
                 con.disconnect(); // connection just moved to pool
             }
         }
 
-        public void post(String messageType, String content) throws JSONException, IOException {
+        public void post(String messageType, String content) throws JSONException, IOException
+        {
             messageOut.put("content", content);
             post(messageType);
         }
     }
 
-    private class MyLocationResult extends LocationResult {
+    private class MyLocationResult extends LocationResult
+    {
         private final CountDownLatch locationLatch = new CountDownLatch(1);
         private Location location = null;
+        private static final int ROUND6 = 1000000;
 
         @Override
-        public void gotLocation(Location location) {
+        public void gotLocation(Location location)
+        {
             this.location = location;
             locationLatch.countDown(); // release await in getJsonLocation
         }
 
-        public JSONObject getJsonLocation() throws JSONException, InterruptedException {
-            // todo round and convert or maybe client is enough...
+        public JSONObject getJsonLocation() throws JSONException, InterruptedException
+        {
             locationLatch.await(2, TimeUnit.MINUTES); // wait max 2 minutes
             JSONObject loc = new JSONObject();
             if (location == null) throw new InterruptedException();
-            loc.put("latitude", location.getLatitude());
-            loc.put("longitude", location.getLongitude());
-            loc.put("accuracy", location.getAccuracy());
+            loc.put("latitude", (double) round(location.getLatitude() * ROUND6) / ROUND6); // 6 decimals = 1m accuracy
+            loc.put("longitude", (double) round(location.getLongitude() * ROUND6) / ROUND6);
+            loc.put("accuracy", (float) round(location.getAccuracy() * 10) / 10); // 1 decimal
             loc.put("altitude", location.getAltitude());
             loc.put("altitudeAccuracy", "-"); // not supported in Android
             loc.put("heading", location.getBearing());
